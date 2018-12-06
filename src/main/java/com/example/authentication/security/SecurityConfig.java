dfd56076d77c758fec09e11bbbda213ed722f426
Bartek Mike <bartlomiej.mike@codlake.com>
2018-12-06 09:45:59 +0100

    Code review and fix

--- src/main/java/com/example/authentication/security/SecurityConfig.java
+++ src/main/java/com/example/authentication/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.authentication.security;
 
+import com.example.authentication.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -17,16 +18,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // userDetailsService
 
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//
+    // UserDetailsService jest interfejsem, który ma wiele implementacji, a CustomUserDetailsService jest jedną z nich.
+    // Wstrzykując zależność typu UserDetailsService, Spring nie wiedział, której implementacji ma użyć.
+    // Dlatego trzeba zadeklarować konkretny typ, albo użyć adnotacji @Qualifier - https://www.logicbig.com/tutorials/spring-framework/spring-core/inject-bean-by-name.html
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encodePWD());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                // Jeśli używamy UserDetailsService, zamiast 'hasAnyRole' oraz 'hasRole', trzeba użyć 'hasAnyAuthority' oraz 'hasAuthority'
+                .antMatchers("/*/floor1/**").hasAnyAuthority("ADMIN","USER")
+                .antMatchers("/*/floor2/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encodePWD() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+    // IN MEMORY AUTH
+
 //    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(encodePWD());
+//    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
+//                .and()
+//                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
+//                .and()
+//                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
 //    }
 //
 //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
+//    protected void configure(final HttpSecurity http) throws Exception {
 //        http
 //                .csrf().disable()
 //                .authorizeRequests()
@@ -37,36 +71,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
 //
 //    @Bean
-//    public BCryptPasswordEncoder encodePWD() {
+//    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
 //    }
-
-    // IN MEMORY AUTH
-
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
-                .and()
-                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/*/floor1/**").hasAnyRole("ADMIN","USER")
-                .antMatchers("/*/floor2/**").hasRole("ADMIN")
-                .and()
-                .httpBasic();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 }
